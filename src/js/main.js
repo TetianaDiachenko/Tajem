5e70312973ca7059e4ea7f38d54b6889afbc4986
Tetiana <tetiana@andrewpcs-MacBook-Pro.local>
2019-04-22 13:47:41 +0300

    mobnav js

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,49 @@
-// slick.js http://kenwheeler.github.io/slick/
-
-$('#slider1').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: true,
-});
-
-$('#slider2').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  focusOnSelect: true,
-  dots: true,
-  prevArrow: $('.people__nav_prev'),
-  nextArrow: $('.people__nav_next'),
-
-});
-
-// Video
-	
-$('.video__button').click(function(){
-		$('.video__button').toggleClass('none'); 
-		$('.video__watch').css('display','block');
-	});
-
 // Mobnav
-function openNav() {
-  document.getElementById("mobNav").style.height = "100%";
+var btn = document.body.querySelector('.mobnav__button');
+var btnClose = document.body.querySelector('.closebtn');
+var mobMenu = document.body.querySelector('#mobNav');
+var mobMenuLi = document.querySelectorAll('.mobnav__main__ul__li');
+
+btn.onclick = function () {
+  mobMenu.style.height = '100%';
 }
-function closeNav() {
-  document.getElementById("mobNav").style.height = "0%";
+
+btnClose.onclick = function () {
+  mobMenu.style.height = '0%';
 }
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
+
+Array.prototype.forEach.call( mobMenuLi, function(el) {      
+  el.onclick = function(e) {
+    mobMenu.style.height = '0%';
+  }
+});
+
+
+
+$(function () {  
+
+// slick.js http://kenwheeler.github.io/slick/
+  $('#slider1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  });
+
+  $('#slider2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    dots: true,
+    prevArrow: $('.people__nav_prev'),
+    nextArrow: $('.people__nav_next'),
+
+  });
+
+  // Video
+  	
+  $('.video__button').click(function(){
+  		$('.video__button').toggleClass('none'); 
+  		$('.video__watch').css('display','block');
+  	});
+})
